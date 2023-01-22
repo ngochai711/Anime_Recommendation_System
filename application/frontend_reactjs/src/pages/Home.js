8d5bb4be353181e4f97d12bdea4b0cd8628fd76f
phatlam1811@gmail.com <phatlam1811@gmail.com>
2023-01-22 21:01:20 +0700

    styling - added title style

--- application/frontend_reactjs/src/pages/Home.js
+++ application/frontend_reactjs/src/pages/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
                     <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
                     <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
                     <div>
-                        <span className="selected-span"></span>
+                        <span className="selected"></span>
                         <span></span>
                         <span></span>
                         <span></span>
@@ -28,8 +28,13 @@ export default function Home() {
                         <span></span>
                     </div>
                 </div>
-                <div className='weekly-top-anime'>
-                    <p>Home</p>
+                <div className="weekly-top-anime">
+                    <div className="title-1">
+                        <div>
+                            <h1>TOP 10 ANIME THIS WEEK</h1>
+                            <div className="line-1"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
