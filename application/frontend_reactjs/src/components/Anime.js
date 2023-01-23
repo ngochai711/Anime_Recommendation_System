8b057192abe5795c62f3bb742143410287675a39
phatlam1811@gmail.com <phatlam1811@gmail.com>
2023-01-23 09:46:56 +0700

    Anime component

---
+++ application/frontend_reactjs/src/components/Anime.js
@@ -0,0 +1,15 @@
+import '../styles/anime.css'
+
+export default function Anime({ poster, title }) {
+    return (
+        <div className="anime">
+            <div className="anime-pic">
+                <img src={poster} alt="poster"></img>
+            </div>
+            <div className="add-to-favorite">
+                <button >+ Add To Favorite</button>
+            </div>
+            <div className="anime-title">{title}</div>
+        </div>
+    );
+}
--- application/frontend_reactjs/src/pages/Home.js
+++ application/frontend_reactjs/src/pages/Home.js
@@ -1,38 +1,52 @@
+import Anime from '../components/Anime'
 import '../styles/home.css';
 
 export default function Home() {
     return (
         <div className="page-content">
             <div className="container">
-                <div className="poster-slide">
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster"></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
-                    <div>
-                        <span className="selected"></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                <div className="page-section">
+                    <div className="poster-slide">
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster"></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <img src={process.env.PUBLIC_URL + '/DemonSlayer_Pos.jpg'} alt="poster" style={{ display: "none" }}></img>
+                        <div>
+                            <span className="selected"></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
                     </div>
                 </div>
-                <div className="weekly-top-anime">
-                    <div className="title-1">
-                        <div>
-                            <h1>TOP 10 ANIME THIS WEEK</h1>
-                            <div className="line-1"></div>
+                <div className="page-section">
+                    <div className="weekly-top-anime">
+                        <div className="title-1">
+                            <div>
+                                <h1>TOP 10 ANIME THIS WEEK</h1>
+                                <div className="line-1"></div>
+                            </div>
+                        </div>
+                        <div className="anime-list">
+                            <Anime poster="https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/d48d4a62b0ac6381c87bd040b69b0a89.jpe" title="Kimetsu no Yaiba"/>
+                            <Anime poster="https://animet.net/upload/images/2019/6/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-thumbnail.jpg" title="Youkoso Jitsuryoku Shugi Shijou"/>
+                            <Anime poster="https://animet.net/upload/images/2019/6/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-thumbnail.jpg" title="Youkoso Jitsuryoku Shugi Shijou"/>
+                            <Anime poster="https://animet.net/upload/images/2019/6/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-thumbnail.jpg" title="Youkoso Jitsuryoku Shugi Shijou"/>
+                            <Anime poster="https://animet.net/upload/images/2019/6/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-thumbnail.jpg" title="Youkoso Jitsuryoku Shugi Shijou"/>
+                            <Anime poster="https://animet.net/upload/images/2019/6/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-thumbnail.jpg" title="Youkoso Jitsuryoku Shugi Shijou"/>
+                            <Anime poster="https://animet.net/upload/images/2019/6/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e-tv-thumbnail.jpg" title="Youkoso Jitsuryoku Shugi Shijou"/>
                         </div>
                     </div>
                 </div>
