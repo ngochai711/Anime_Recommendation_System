3aa282b0bd43ae1300930f49d6d947ef647abed0
Khanh Nguyen <khanhnguyen02311@gmail.com>
2023-02-03 15:03:47 +0700

    minor changes

--- application/backend_flask/components/blueprints/admin/server.py
+++ application/backend_flask/components/blueprints/admin/server.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, request, jsonify
-from flask_cors import CORS
 import platform
 
 bpserver = Blueprint('bpserver', __name__)
-CORS(bpserver)
+
 
 @bpserver.route('/serverinfo', methods = ['GET'])
 def serverinfo():
